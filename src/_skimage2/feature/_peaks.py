b0ed90ad37ceadefc79a107752e9e3f89a09e5aa
Steven Silvester <steve.silvester@mongodb.com>
2026-04-06 07:13:07 -0500

    Update src/_skimage2/feature/_peaks.py
    Co-authored-by: Lars Grüter <lagru+github@mailbox.org>

--- src/_skimage2/feature/_peaks.py
+++ src/_skimage2/feature/_peaks.py
@@ -340,7 +340,9 @@ def peak_local_max(
     >>> ski2.feature.peak_local_max(image)
     array([[3, 2],
            [3, 4]])
-    >>> # Ensure peaks are at least 2 pixels apart
+
+    Ensure peaks are at least 2 pixels apart
+
     >>> ski2.feature.peak_local_max(image, min_distance=2)
     array([[3, 2]])
     >>> # Allow peaks on the image border
